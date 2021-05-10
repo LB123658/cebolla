89eda9cfc7fc37e4b35c0ee9bd220fc2283197e4
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-05-10 13:17:38 -0500

    Create results.js

---
+++ results.js
@@ -0,0 +1,95 @@
+if (location.search.split("=")[1] == "google+classroom") {
+  document.getElementById("com").innerHTML = "Google Classroom - official site<span class='material-icons' style='color:red; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://classroom.google.com/h";
+} else if (location.search.split("=")[1] == "google+Classroom") {
+  document.getElementById("com").innerHTML = "Google Classroom - official site<span class='material-icons' style='color:red; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://classroom.google.com/h";
+} else if (location.search.split("=")[1] == "Google+classroom") {
+  document.getElementById("com").innerHTML = "Google Classroom - official site<span class='material-icons' style='color:red; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://classroom.google.com/h";
+} else if (location.search.split("=")[1] == "Google+Classroom") {
+  document.getElementById("com").innerHTML = "Google Classroom - official site<span class='material-icons' style='color:red; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://classroom.google.com/h";
+} else if (location.search.split("=")[1] == "google+maps") {
+  document.getElementById("com").innerHTML = "Google Maps - official site<span class='material-icons' style='color:red; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://maps.google.com";
+} else if (location.search.split("=")[1] == "google+Maps") {
+  document.getElementById("com").innerHTML = "Google Maps - official site<span class='material-icons' style='color:red; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://maps.google.com";
+} else if (location.search.split("=")[1] == "Google+maps") {
+  document.getElementById("com").innerHTML = "Google Maps - official site<span class='material-icons' style='color:red; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://maps.google.com";
+} else if (location.search.split("=")[1] == "Google+Maps") {
+  document.getElementById("com").innerHTML = "Google Maps - official site<span class='material-icons' style='color:red; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://maps.google.com";
+} else if (location.search.split("=")[1] == "google+drive") {
+  document.getElementById("com").innerHTML = "Google Drive - official site<span class='material-icons' style='color:#ffc000; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://drive.google.com";
+} else if (location.search.split("=")[1] == "google+Drive") {
+  document.getElementById("com").innerHTML = "Google Drive - official site<span class='material-icons' style='color:#ffc000; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://drive.google.com";
+} else if (location.search.split("=")[1] == "Google+drive") {
+  document.getElementById("com").innerHTML = "Google Drive - official site<span class='material-icons' style='color:#ffc000; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://drive.google.com";
+} else if (location.search.split("=")[1] == "Google+Drive") {
+  document.getElementById("com").innerHTML = "Google Drive - official site<span class='material-icons' style='color:#ffc000; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://drive.google.com";
+} else if (location.search.split("=")[1] == "google+docs") {
+  document.getElementById("com").innerHTML = "Google Docs - create and edit documents<span class='material-icons' style='color:red; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://docs.google.com";
+} else if (location.search.split("=")[1] == "google+Docs") {
+  document.getElementById("com").innerHTML = "Google Docs - create and edit documents<span class='material-icons' style='color:red; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://docs.google.com";
+} else if (location.search.split("=")[1] == "Google+docs") {
+  document.getElementById("com").innerHTML = "Google Docs - create and edit documents<span class='material-icons' style='color:red; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://docs.google.com";
+} else if (location.search.split("=")[1] == "Google+Docs") {
+  document.getElementById("com").innerHTML = "Google Docs - create and edit documents<span class='material-icons' style='color:red; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://docs.google.com";
+} else if (location.search.split("=")[1] == "google+sites") {
+  document.getElementById("com").innerHTML = "Google Sites - official site<span class='material-icons' style='color:red; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://sites.google.com";
+} else if (location.search.split("=")[1] == "google+Sites") {
+  document.getElementById("com").innerHTML = "Google Sites - official site<span class='material-icons' style='color:red; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://sites.google.com";
+} else if (location.search.split("=")[1] == "Google+sites") {
+  document.getElementById("com").innerHTML = "Google Sites - official site<span class='material-icons' style='color:red; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://sites.google.com";
+} else if (location.search.split("=")[1] == "Google+Sites") {
+  document.getElementById("com").innerHTML = "Google Sites - official site<span class='material-icons' style='color:red; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://sites.google.com";
+} else if (location.search.split("=")[1] == "google+slides") {
+  document.getElementById("com").innerHTML = "Google Slides - official site<span class='material-icons' style='color:red; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://slides.google.com";
+} else if (location.search.split("=")[1] == "google+Slides") {
+  document.getElementById("com").innerHTML = "Google Slides - official site<span class='material-icons' style='color:red; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://slides.google.com";
+} else if (location.search.split("=")[1] == "Google+slides") {
+  document.getElementById("com").innerHTML = "Google Slides - official site<span class='material-icons' style='color:red; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://slides.google.com";
+} else if (location.search.split("=")[1] == "Google+Slides") {
+  document.getElementById("com").innerHTML = "Google Slides - official site<span class='material-icons' style='color:red; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://slides.google.com";
+} else if (location.search.split("=")[1] == "google+slides") {
+  document.getElementById("com").innerHTML = "Google Slides - official site<span class='material-icons' style='color:red; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://slides.google.com";
+} else if (location.search.split("=")[1] == "instagram") {
+  document.getElementById("com").innerHTML = "Instagram - official site<span class='material-icons' style='color:red; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://instagram.com";
+} else if (location.search.split("=")[1] == "Instagram") {
+  document.getElementById("com").innerHTML = "Instagram - official site<span class='material-icons' style='color:red; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://instagram.com";
+} else if (location.search.split("=")[1] == "insta") {
+  document.getElementById("com").innerHTML = "Instagram - official site<span class='material-icons' style='color:red; font-size: 30px;' title='Poor privacy practices and tracking'>person_search</span>";
+  document.getElementById("comA").href = "https://instagram.com";
+} 
+
+
+
+
+
+
+
+
+
+
